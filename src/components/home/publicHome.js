ae1583e6f222870cc8dbc80f21ad0c621eefb715
santiago <santiago.ottolini98@gmail.com>
2020-04-29 12:50:56 -0300

    SA-FE(TEST)HomeCSS

--- src/components/home/publicHome.js
+++ src/components/home/publicHome.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
+import "./publicHome.css"
 
 class publicHome extends Component {
   render() {
@@ -12,39 +13,19 @@ class publicHome extends Component {
           <div className='title'>
             <h1>SchoolApp</h1>
           </div>
-          <div className='publicity'>
-            <div className='publicity-mr'>
-            </div>
-          </div>
         </div>
-        <div className='logged'>
-          <div className='options'>
-            <div className='homeMenu'>
-              <Link to='/publicHome'>Home</Link>
-            </div>
-            <div className='productMenu'>
-              <Link to='/publicProduct'>Product</Link>
-            </div>
-            <div className='categorieMenu'>
-              <Link to='/categorie'>Categorie</Link>
-            </div>
-            <div className='basketMenu'>
-              <Link to='/cart'>Cart</Link>
-            </div>
+        <div className='home'>
+          <div className='login'>
+            <button><Link to='/login'>Login</Link></button>
           </div>
-          <div className='buttonSession'>
-            <div className='loginMenu'>
-              <Link to='/login'>Login</Link>
-            </div>
-            <div className='registerMenu'> 
-              <Link to='/register'>Register</Link>
-            </div>
+          <div className='register'>
+            <button> <Link to='/register'>Register</Link></button>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-md-8'>
+          <div className='information'>
+            <button><Link to='/information'>information</Link></button>
           </div>
-          <div id='col-md-4'>
+          <div className='contact'>
+            <button><Link to='/contact'>Contacto</Link></button>
           </div>
         </div>
       </div>
