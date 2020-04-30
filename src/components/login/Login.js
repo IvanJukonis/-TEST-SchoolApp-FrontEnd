07d49b265b37bfbcef788d8d58f5e799528f86c8
IvanJukonis <IvanJukonis@hotmail.com>
2020-04-30 12:04:54 -0300

    SA-FE(TEST): Register Path

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,7 +2,7 @@ import "./login.css";
 import React, { Component } from "react";
 import { Formik, Form, Field } from "formik";
 import { connect } from "react-redux";
-import { logIn } from "../../redux/actions/login";
+import { logIn } from "../../redux/actions/user";
 import { bindActionCreators } from "redux";
 import { Link } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
