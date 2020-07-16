402eec711b4d1201c9972d275965195b59688676
santiago <santiago.ottolini98@gmail.com>
2020-07-16 18:56:37 -0300

    AC-FE: Add semicolon

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-import React, { Component } from "react"
-import {Provider} from "react-redux"
+import React, { Component } from "react";
+import { Provider } from "react-redux";
 //Bring the store
-import store from "./redux/store/store"
+import store from "./redux/store/store";
 //Bring the roads
-import Routes from "./routes/view"
+import Routes from "./routes/view";
 
-class App extends Component{
+class App extends Component {
   render() {
     return (
-        //Provider habilita el store para todas las rutas
-    <Provider store={store}>
-      <Routes/>
-    </Provider>
-    )
-    
+      //Provider habilita el store para todas las rutas
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    );
   }
 }
 //SE EXPORTA "App"
